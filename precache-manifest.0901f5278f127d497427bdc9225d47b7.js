37d189211d55f8bd0cef8e00ac02efd3f63dd4a1
Vitalii <vitaliybondarenko1982@gmail.com>
2020-03-24 07:39:15 +0200

    Initial dist subtree commit

---
+++ precache-manifest.0901f5278f127d497427bdc9225d47b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f394341413de4100b20e",
+    "url": "/vue-form-test-task/css/app.2a709ce1.css"
+  },
+  {
+    "revision": "1b3be0d29586e1e1e14add8d0e936fdc",
+    "url": "/vue-form-test-task/index.html"
+  },
+  {
+    "revision": "f394341413de4100b20e",
+    "url": "/vue-form-test-task/js/app.952268eb.js"
+  },
+  {
+    "revision": "43363647fea4cdd8702a",
+    "url": "/vue-form-test-task/js/chunk-vendors.c5ddd902.js"
+  },
+  {
+    "revision": "8eafa0047d77580128016826ee988852",
+    "url": "/vue-form-test-task/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-form-test-task/robots.txt"
+  }
+]);
